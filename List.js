cf70eb5da56ab7e445566f23642527845ffd5219
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-10-25 19:02:43 +0530

    Add files via upload

---
+++ List.js
@@ -0,0 +1,12 @@
+import Compforlist from "../Components/Compforlist";
+
+
+
+export default function FirstList(){
+    return(
+        <>
+        <Compforlist/>
+        <h3>List Page</h3>
+        </>
+    )
+}
