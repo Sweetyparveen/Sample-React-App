e2aa3e1f2073643b13c7eea8a5e0bf29445fcb41
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-10-26 16:01:10 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,11 @@
+import Compforlist from "../Components/Compforlist";
+
+
+
+export default function AddPage(){
+    return(
+        <>
+        <h2>This is the Add Page</h2>
+        </>
+    )
+}
