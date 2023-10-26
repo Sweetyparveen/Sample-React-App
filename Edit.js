706c1a639df1b561f003b237428f832be4846074
Sweetyparveen <148330117+Sweetyparveen@users.noreply.github.com>
2023-10-26 16:01:33 +0530

    Add files via upload

---
+++ Edit.js
@@ -0,0 +1,11 @@
+import Compforlist from "../Components/Compforlist";
+
+
+
+export default function EditPage(){
+    return(
+        <>
+        <h2>This is the Edit Page</h2>
+        </>
+    )
+}
